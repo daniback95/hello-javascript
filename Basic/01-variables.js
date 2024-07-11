845d68cd17f86c98bbd8c46f02aade5cf3376dd4
Daniel Baracaldo <77088158+DaniBack95@users.noreply.github.com>
2024-07-11 12:00:14 -0500

    Clase 1 | 10/07/2024

---
+++ Basic/01-variables.js
@@ -0,0 +1,20 @@
+// var, no recomenda, en desuso por temas de hoisting
+var helloWorld = "Hola JavaSript";
+console.log(helloWorld);
+helloWorld = "Hola de nuevo JavaScript";
+console.log(helloWorld);
+
+// let
+let helloWorld2 = "Hola JavaScript 2";
+console.log(helloWorld2);
+helloWorld2 = "Hola de nuevo JavaScript 2"
+console.log(helloWorld2);
+
+// const
+const helloWorld3 = "Hola JavaScript 3"
+console.log(helloWorld3);
+// Error, const no permite reasignación de valores
+/* 
+helloWorld3 = "Hola de nuevo JavaScript 3"
+console.log(helloWorld3)
+ */
