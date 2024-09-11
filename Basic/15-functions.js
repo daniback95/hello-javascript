664ea39ebfbb6e7e1aa2bfb58523c5619fc8ba98
Daniel Baracaldo <77088158+daniback95@users.noreply.github.com>
2024-09-11 17:02:12 -0500

    Funciones: Declaradas, expresadas, parámetros y argumentos

--- Basic/15-functions.js
+++ Basic/15-functions.js
@@ -1,48 +1,97 @@
-// Funciones
+/* Funciones */
 
-// simple
-function myFunc() {
-  console.log("Hola función");
+/* 
+sintaxis
+  function nombre (parametro1, parametro2, ...) {
+    cuerpo de la fucion
+    return
+  }
+
+  Los parametro son opcinales en caso de que la funcion
+  necesite datos adicionales para ejecutar el código, estos
+  parámetros es la declaración de variables que posteriormente
+  tomarán el valor que se le pase cuando la función
+  sea invocada, este valor que se le pasa a la función
+  se le conoce como argumento
+
+  return es opcional, permite devolver un valor, 
+  este valor puede ser cualquier tipo de dato sea
+  primitivo o complejo, incluso puede retornar otra
+  función
+*/
+
+/* Declaración */
+
+function saludar() {
+  console.log("Hola desde una función");
 }
-myFunc();
+saludar(); // Invocación de la función
 
-// con parámetros y argumentos
-function myFuncParams(namme) {
-  console.log(`Hola ${namme}!`);
+/* Parámetros y argumentos */
+
+function saludarParametros(nombre, edad) {
+  console.log(`Hola soy ${nombre}!, tengo ${edad} años`);
 }
-myFuncParams("Dani");
-myFuncParams("Brais");
+saludarParametros('Freddy', 37); // Aquí le pasamos los argumentos (valores)
 
-// parámetro por defecto
-function sum(a, b) {
-  console.log(a + b);
+/* Parámetros predeterminados / por defecto */
+
+function saludarPredeterminados(nombre = 'Desconocido', edad = 0) {
+  console.log(`Hola soy ${nombre}!, tengo ${edad} años`)
 }
+saludarPredeterminados()
+saludarPredeterminados(undefined, 7)
+saludarPredeterminados('Felipe')
 
-sum(3, 9);
-sum(3);
-sum();
+/* Retorno de valores */
 
-function defaultSum(a = 0, b = 0) {
-  return a + b;
+function multiplicar(a, b) {
+  return a * b
 }
+let resultado = multiplicar(3, 9) // El valor retornado es asignado a una variable
+console.log(`El resulado de la multiplicación es: ${resultado}`)
 
-defaultSum();
-defaultSum(3);
-defaultSum(9, 3);
-defaultSum(9, 12);
+/* Funciones declaradas vs expresadas */
 
-// return
-function mult(a, b) {
-  return a * b;
+/* 
+  Una declaración es una instrucción para hacer algo,
+  como crear una varible, definir una función o 
+  iniciar un bucle.
+  Necesariamente cuando se declara algo se debe darle un 
+  nombre e indicar lo que debe hacer.
+  Cuando declaramos una función se le da un nombre
+  y se le indica las intrucciones de lo que debe hacer.
+  El motor de JavaScript aplica algo llamado "Hoisting" a las variables
+  var y a las funciones declaradas, esto quiere decir que lo primero 
+  que hace el motor es elevar al comienzo del ámbito las funciones y las
+  variables var permitiendo que estas puedan ser invocadas antes de su
+  definición.
+*/
+
+declarada() // Invocación antes de su definición
+function declarada() { // declaración de la función
+  console.log('Hola, soy una función declarada, puedo ser invocada antes de mi definición o despúes')
 }
-let result = mult(3, 5);
-console.log(result);
+declarada() // Invocación después de su definición
 
-// Anómimas
-const anonima = function (namme) {
-  console.log(`Hola ${namme}!`);
-};
-anonima("Gabriel");
+/* 
+  Las expresiones es todo aquel código que produce un valor, 
+  desde una operación aritmética, un valor por sí mismo, 
+  o una función asignada a una variable, las expresiones
+  no requieren de un nombre, a estas expresiones no se les
+  aplica el hoisting, lo que significa que no se pueden
+  invocar antes de su definición/declaración.
+*/
+/* 
+  Aquí la función no necesita un nombre, a esto se le
+  conoce cómo funciones anónimas
+*/
+
+// funcionExpresada() esto genera un error: Cannot access 'funcionExpresada' before initialization
+let funcionExpresada = function() { 
+  console.log('Hola soy una función expresada, no puedo ser invocada antes de mi declaración')
+}
+funcionExpresada() // Invocación después de su declaración
 
 // Arrow functions
 const arrowFunc = (namme) => {
@@ -66,7 +115,7 @@ function applyfunc(fun, a, b) {
   let res = fun(a, b) * 2;
   console.log(res);
 }
-applyfunc(defaultSum, 3, 9);
+//applyfunc(defaultSum, 3, 9);
 
 // ForEach
 let movies = [
